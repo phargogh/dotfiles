ba40de6e1fa1be1fa86bee42332a3f98fb901ebb
James Douglass <jamesdouglassusa@gmail.com>
2020-04-09 10:07:28 -0700

    Adding patch for zsh git prompt to show tag if at tag.

---
+++ oh-my-zsh/my_paches.zsh
@@ -0,0 +1,11 @@
+
+# Override default git prompt to show the tag name if we're at a tag.
+function git_prompt_info() {
+  local ref
+  if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
+    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+    ref=$(command git tag --points-at HEAD 2> /dev/null) || \
+    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  fi
+}
