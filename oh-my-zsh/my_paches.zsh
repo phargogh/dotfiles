d20e8fa822b923f3a0dca5f62cfae0c8d25509cc
James Douglass <jamesdouglassusa@gmail.com>
2020-04-24 14:14:33 -0700

    Adding github git repo path.

--- oh-my-zsh/my_paches.zsh
+++ oh-my-zsh/my_paches.zsh
@@ -6,9 +6,10 @@
 function git_prompt_info() {
   local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
+    github_repo=$(command git remote get-url origin | egrep -o '[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+.git$' | sed 's|.git$||g')
     ref=$(command git describe --exact-match --tags 2> /dev/null) || \
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${github_repo}: ${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
