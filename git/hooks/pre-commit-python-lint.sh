533236ff3867ef9f939a30b1a4d515a0fab127ab
James Douglass <jamesdouglassusa@gmail.com>
2020-04-24 13:40:30 -0700

    Adding YML linting.

--- git/hooks/pre-commit-python-lint.sh
+++ git/hooks/pre-commit-python-lint.sh
@@ -10,9 +10,18 @@ for pythonfile in $(git diff --name-only --cached | egrep '.*\.py$')
 do
         echo "Linting $pythonfile"
         $FLAKE8_KILL $pythonfile || ERRORS_PRESENT=$(($ERRORS_PRESENT | $?))
-        $PYCODESTYLE $pythonfile || ERRORS_PRESENT=$(($ERRORS_PRESENT | $?))
+        #$PYCODESTYLE $pythonfile || ERRORS_PRESENT=$(($ERRORS_PRESENT | $?))
         $PYDOCSTYLE $pythonfile || ERRORS_PRESENT=$(($ERRORS_PRESENT | $?))
 done
+
+echo "Pre-commit: Checking staged YML files for syntax errors."
+for ymlfile in $(git diff --name-only --cached | egrep '.*\.ya?ml$')
+do
+        echo "Checking $ymlfile"
+        # Found this at https://stackoverflow.com/a/20420243/299084
+        ruby -ryaml -e "p YAML.load(STDIN.read)" < $ymlfile
+done
+
 if [[ $ERRORS_PRESENT -gt 0 ]]
 then
         # Read user input, assign stdin to keyboard
